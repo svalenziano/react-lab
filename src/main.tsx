16d530b211e63cb1c3cdc52fa467417a5155b977
SValenziano <senorvalenz@gmail.com>
2025-11-14 12:45:29 -0500

    Simple list with working `key`

--- src/main.tsx
+++ src/main.tsx
@@ -1,49 +1,21 @@
 import { StrictMode, type MouseEventHandler } from 'react';
 import { createRoot } from 'react-dom/client';
 import { useState } from 'react';
-import './sv.css'
-// import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <MyApp />
-  </StrictMode>,
+    <StrictMode>
+        <DoggoHerd />
+    </StrictMode>,
 )
 
-function MyButton({count, clickFunc: f}: {count: number, clickFunc: MouseEventHandler}) {
-
-    return (
-        <button className={String(count)} onClick={f}>I'm a button! {count}</button>
-    )
+function Doggo({num}) {
+  return <h1>Doggo number {num}</h1>
 }
 
-function MyApp() {
-    const [count, setter] = useState(0);
-
-    const increment = () => setter(count + 1);
-
-    return (
-        <div className='boring'>
-        <h2>What a great app!</h2>
-        <p>How is <br/><br/> ze life?</p>
-        <MyButton count={count} clickFunc={increment} />
-        <MyButton count={count} clickFunc={increment} />
-        <MyButton count={count} clickFunc={increment} />
-        </div>
-    )
-}
-
-const stuff = [
-    {place: "Durham, NC", data: 1234},
-    {place: "Paris, France", data: 4567},
-    {place: "Taipei, Taiwan", data: 8901},
-]
-
-function DataList() {
-    const items = stuff.map(i => <li>{i.place} 👉 {i.data} </li>);
-    return (
-        <ul>
-          {items}
-        </ul>
-    )
+function DoggoHerd() {
+  const result = [];
+  for (let i=0; i < Math.random() * 12; i++) {
+    result.push(<Doggo num={i} key={i}/>)
+  }
+  return <div>{result}</div>
 }
